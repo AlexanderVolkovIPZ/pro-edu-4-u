cf6d89ef6d1a9ba621cad08bbbd43944e26f6155
Volkov <volkov.aliksandr0203@gmail.com>
2024-08-20 23:55:31 +0300

    Added auth providers

---
+++ auth.ts
@@ -0,0 +1,47 @@
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
+import Google from "next-auth/providers/google";
+import Github from "next-auth/providers/github";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+    Github({
+      clientId: process.env.AUTH_GITHUB_ID!,
+      clientSecret: process.env.AUTH_GITHUB_SECRET!,
+    }),
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID!,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET!,
+    }),
+    Credentials({
+      credentials: {
+        email: { label: "email", type: "text" },
+        password: { label: "password", type: "password" },
+      },
+      authorize: async (credentials) => {
+        const email = credentials?.email;
+        const password = credentials?.password;
+        if (!email || !password) {
+          throw new Error("Invalid credentials");
+        }
+
+        const user = await prismaDb?.user.findUnique({
+          where: {
+            email,
+          },
+        });
+        if (!user || !user?.password) {
+          throw new Error("Invalid credentials");
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+        if (!isPasswordCorrect) {
+          throw new Error("Invalid credentials");
+        }
+
+        return user;
+      },
+    }),
+  ],
+});
