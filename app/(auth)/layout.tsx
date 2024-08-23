327662eeb2d3b19d2677839a8f208840d2b8e927
Volkov <volkov.aliksandr0203@gmail.com>
2024-08-24 02:35:46 +0300

    Updated SignUpPage component. Apply zod for SignUpPage

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,8 +1,8 @@
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className='flex flex-col min-h-screen'>
       <div>HEADER</div>
-      <div className="flex flex-1 justify-center items-center">{children}</div>
+      <div className='flex flex-1 justify-center items-center'>{children}</div>
       <div>FOOTER</div>
     </div>
   );
