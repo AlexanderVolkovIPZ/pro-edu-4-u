0aee8ae80cf545576c74ba8e7e0e17fe9a78bda5
Volkov <volkov.aliksandr0203@gmail.com>
2024-08-25 17:33:21 +0300

    Created menuLinksList

---
+++ app/(auth)/_shared/menu-links-list.ts
@@ -0,0 +1,18 @@
+export const menuLinksList = [
+  {
+    title: 'Courses',
+    link: '/courses',
+  },
+  {
+    title: 'Resources',
+    link: '/resources',
+  },
+  {
+    title: 'Community',
+    link: '/community',
+  },
+  {
+    title: 'About Us',
+    link: '/about',
+  },
+];
