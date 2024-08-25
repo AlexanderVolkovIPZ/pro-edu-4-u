bf3af65d6d5ae0c420b760ff0497141eb3446279
Volkov <volkov.aliksandr0203@gmail.com>
2024-08-25 17:41:45 +0300

    Created Header component

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,9 @@
+import Header from './_components/header';
+
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className='flex flex-col min-h-screen'>
-      <div>HEADER</div>
+      <Header />
       <div className='flex flex-1 justify-center items-center'>{children}</div>
       <div>FOOTER</div>
     </div>
