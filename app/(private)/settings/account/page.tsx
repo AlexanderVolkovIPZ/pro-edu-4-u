cc035151a6936fcaf3dd0a159068ebc177f8804d
Volkov <volkov.aliksandr0203@gmail.com>
2024-10-16 23:52:51 +0300

    Removed react routing and used next routing

---
+++ app/(private)/settings/account/page.tsx
@@ -0,0 +1,7 @@
+import Account from './_components/account';
+
+const AccountPage = () => {
+  return <Account />;
+};
+
+export default AccountPage;
--- next.config.mjs
+++ next.config.mjs
@@ -14,14 +14,6 @@ const nextConfig = {
       },
     ],
   },
-  async rewrites() {
-    return [
-      {
-        source: '/settings/:path*',
-        destination: '/settings',
-      },
-    ];
-  },
 };
 
 export default nextConfig;
