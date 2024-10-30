d0a66b2851f1e8715cd1e5c5d9e27359d4a8de1e
Volkov <volkov.aliksandr0203@gmail.com>
2024-10-30 02:23:51 +0200

    Created getAuction action

---
+++ app/actions/get-auction.ts
@@ -0,0 +1,22 @@
+type AuctionProps = {
+  auctionId: string;
+};
+export const getAuction = async ({ auctionId }: AuctionProps) => {
+  try {
+    const auction = await prismaDb?.auction.findUnique({
+      where: {
+        id: auctionId,
+      },
+    });
+
+    if (!auction) return null;
+
+    return {
+      ...auction,
+      startDate: auction.startDate?.toUTCString(),
+      endDate: auction.endDate?.toUTCString(),
+    };
+  } catch {
+    return null;
+  }
+};
