42d3a2d6d0fb6b74cd10d621e85344a4b45e1ebe
Volkov <volkov.aliksandr0203@gmail.com>
2024-11-03 18:02:27 +0200

    Created auction queries

--- app/queries/auction.ts
+++ app/queries/auction.ts
@@ -1,8 +1,10 @@
 'use client';
 
 import { Auction } from '@prisma/client';
-import { useMutation, UseMutationResult } from '@tanstack/react-query';
+import { useMutation, UseMutationResult, useQuery, UseQueryResult } from '@tanstack/react-query';
 import axios, { AxiosResponse } from 'axios';
+import { queryClient } from '../providers/query-client-provider';
+import { AUCTION } from './query-keys';
 
 export function useCreateAuction<T extends Pick<Auction, 'title'>>(): UseMutationResult<
   AxiosResponse<Auction>,
@@ -17,10 +19,23 @@ export function useCreateAuction<T extends Pick<Auction, 'title'>>(): UseMutatio
   });
 }
 
-export function useUpdatedAuction<T>(): UseMutationResult<AxiosResponse<T>, Error, T> {
+export function useUpdateAuction<T>(auctionId: string): UseMutationResult<AxiosResponse<T>, Error, T> {
   return useMutation<AxiosResponse<T>, Error, T>({
     mutationFn: async (data: T) => {
-      const response = await axios.put(`${process.env.NEXT_PUBLIC_BASE_URL}/api/auction`, data);
+      const response = await axios.patch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/auction/${auctionId}`, data);
+      return response;
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: [AUCTION, auctionId] });
+    },
+  });
+}
+
+export function useAuction<T>(auctionId: string): UseQueryResult<AxiosResponse<T>, Error> {
+  return useQuery<AxiosResponse<T>, Error>({
+    queryKey: [AUCTION, auctionId],
+    queryFn: async () => {
+      const response = await axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}/api/auction/${auctionId}`);
       return response;
     },
   });
