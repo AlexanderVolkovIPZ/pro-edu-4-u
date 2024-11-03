9451b0f66bf873752d2cbbcb4f1da071f25e9c4e
Volkov <volkov.aliksandr0203@gmail.com>
2024-11-03 17:33:01 +0200

    Updated input component

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -7,55 +7,67 @@ import { useState } from 'react';
 import { Eye, EyeOff } from 'lucide-react';
 export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   error?: FieldError;
+  errorMsgPosition?: 'bottom-left' | 'bottom-right';
 }
 
-const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, type, error = null, ...props }, ref) => {
-  const isPassword = type === 'password';
-  const [shouldShowPassword, setShouldShowPassword] = useState(false);
-
-  return (
-    <div className='relative'>
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, error = null, errorMsgPosition = 'bottom-left', ...props }, ref) => {
+    const isPassword = type === 'password';
+    const [shouldShowPassword, setShouldShowPassword] = useState(false);
+    const getErrorMsgPosition = () => {
+      switch (errorMsgPosition) {
+        case 'bottom-left':
+          return 'left-0';
+        case 'bottom-right':
+          return 'right-0';
+      }
+    };
+    return (
       <div className='relative'>
-        <input
-          type={isPassword ? (shouldShowPassword ? 'text' : 'password') : type}
-          className={cn(
-            'flex h-10 w-full rounded-md border border-gray-400 bg-background  py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 pl-3 pr-8',
-            className,
-            error && 'border-red-500 ',
-            !error && 'focus:border-gray-500'
-          )}
-          ref={ref}
-          {...props}
-        />
-        {isPassword && (
-          <div className='absolute right-1 top-1/2 -translate-y-1/2 cursor-pointer'>
-            {shouldShowPassword ? (
-              <>
-                <EyeOff
-                  color='#6b7280'
-                  strokeWidth='1'
-                  className='hover:scale-110 transition-all'
-                  onClick={() => setShouldShowPassword((prev) => !prev)}
-                />
-              </>
-            ) : (
-              <>
-                <Eye
-                  color='#6b7280'
-                  strokeWidth='1'
-                  className='hover:scale-110 transition-all'
-                  onClick={() => setShouldShowPassword((prev) => !prev)}
-                />
-              </>
+        <div className='relative'>
+          <input
+            type={isPassword ? (shouldShowPassword ? 'text' : 'password') : type}
+            className={cn(
+              'flex h-10 w-full rounded-md border border-gray-400 bg-background  py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 pl-3 pr-8',
+              className,
+              error && 'border-red-500 ',
+              !error && 'focus:border-gray-500'
             )}
-          </div>
+            ref={ref}
+            {...props}
+          />
+          {isPassword && (
+            <div className='absolute right-1 top-1/2 -translate-y-1/2 cursor-pointer'>
+              {shouldShowPassword ? (
+                <>
+                  <EyeOff
+                    color='#6b7280'
+                    strokeWidth='1'
+                    className='hover:scale-110 transition-all'
+                    onClick={() => setShouldShowPassword((prev) => !prev)}
+                  />
+                </>
+              ) : (
+                <>
+                  <Eye
+                    color='#6b7280'
+                    strokeWidth='1'
+                    className='hover:scale-110 transition-all'
+                    onClick={() => setShouldShowPassword((prev) => !prev)}
+                  />
+                </>
+              )}
+            </div>
+          )}
+        </div>
+
+        {error && (
+          <p className={cn('text-xs text-red-500 absolute -bottom-4', getErrorMsgPosition())}>{error.message}</p>
         )}
       </div>
-
-      {error && <p className='text-xs text-red-500 absolute -bottom-4 left-0'>{error.message}</p>}
-    </div>
-  );
-});
+    );
+  }
+);
 Input.displayName = 'Input';
 
 export { Input };
