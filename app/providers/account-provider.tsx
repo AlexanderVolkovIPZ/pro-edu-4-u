2005dd7edefe82c220031d676c0c1553dc2b125c
Volkov <volkov.aliksandr0203@gmail.com>
2024-11-03 17:33:21 +0200

    Updated account provider

--- app/providers/account-provider.tsx
+++ app/providers/account-provider.tsx
@@ -1,24 +1,41 @@
 'use client';
 import { createContext, useEffect, useState } from 'react';
 
+const DEFAULT_LOCALE = 'en';
+const DEFAULT_TIME_ZONE = 'Europe/Kyiv';
+
 type AccountContextType = {
-  locale: string[];
-  setLocale: (locale: string[]) => void;
+  locale: string;
+  setLocale: (locale: string) => void;
+  timeZone: string;
+  setTimeZone: (timeZone: string) => void;
 };
 
 export const AccountContext = createContext<AccountContextType>({
-  locale: ['en'],
+  locale: DEFAULT_LOCALE,
   setLocale: () => {},
+  timeZone: DEFAULT_TIME_ZONE,
+  setTimeZone: () => {},
 });
 
 export const AccountProvider = ({ children }: { children: React.ReactNode }) => {
-  const [locale, setLocale] = useState(['en']);
+  const [locale, setLocale] = useState(DEFAULT_LOCALE);
+  const [timeZone, setTimeZone] = useState(DEFAULT_TIME_ZONE);
 
   useEffect(() => {
     if (typeof window !== 'undefined' && navigator.languages) {
-      setLocale(Array.from(navigator.languages));
+      setLocale(navigator.languages[0] || DEFAULT_LOCALE);
     }
   }, []);
 
-  return <AccountContext.Provider value={{ locale, setLocale }}>{children}</AccountContext.Provider>;
+  useEffect(() => {
+    const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    if (userTimeZone) {
+      setTimeZone(userTimeZone);
+    }
+  }, []);
+
+  return (
+    <AccountContext.Provider value={{ locale, setLocale, timeZone, setTimeZone }}>{children}</AccountContext.Provider>
+  );
 };
