f6b12954cc0ed86bd01afdb5df269aad989a684d
Volkov <volkov.aliksandr0203@gmail.com>
2024-11-03 18:00:55 +0200

    created auction/[auctionId] route

---
+++ app/api/auction/[auctionId]/route.ts
@@ -0,0 +1,52 @@
+import getAuthUser from '@/app/actions/get-auth-user';
+import { NextResponse } from 'next/server';
+import prismaDb from '@/lib/prismadb';
+
+export async function GET(request: Request, { params }: { params: { auctionId: string } }) {
+  const authUser = await getAuthUser();
+  if (!authUser) {
+    return new NextResponse('Unauthorized', { status: 401 });
+  }
+
+  try {
+    const auction = await prismaDb.auction.findFirst({
+      where: {
+        id: params.auctionId,
+      },
+    });
+
+    return NextResponse.json(auction);
+  } catch (error) {
+    console.error('CREATE_AUCTION_ERROR -> ', error);
+    return new NextResponse('Internal server error', { status: 500 });
+  }
+}
+
+export async function PATCH(request: Request, { params }: { params: { auctionId: string } }) {
+  const authUser = await getAuthUser();
+  if (!authUser) {
+    return new NextResponse('Unauthorized', { status: 401 });
+  }
+
+  const body = await request.json();
+  const { title, description, startDate, endDate } = body;
+
+  try {
+    const auction = await prismaDb.auction.update({
+      data: {
+        title,
+        description,
+        startDate,
+        endDate,
+      },
+      where: {
+        id: params.auctionId,
+      },
+    });
+
+    return NextResponse.json(auction);
+  } catch (error) {
+    console.error('CREATE_AUCTION_ERROR -> ', error);
+    return new NextResponse('Internal server error', { status: 500 });
+  }
+}
