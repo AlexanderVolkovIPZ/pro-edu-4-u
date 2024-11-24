59512c9a122c853447cb60e37904efe4e19d881b
Volkov <volkov.aliksandr0203@gmail.com>
2024-11-24 03:24:39 +0200

    Updated spinner component

--- components/spinner.tsx
+++ components/spinner.tsx
@@ -5,10 +5,19 @@ type Spinner = {
   height?: string | number;
   color?: string;
   animate?: string;
+  className?: string;
 };
 
-const Spinner = ({ width = 26, height = 26, color = 'text-secondary', animate = 'animate-spin' }: Spinner) => {
-  return <Loader2 className={`${animate} ${color}`} style={{ width: `${width}px`, height: `${height}px` }} />;
+const Spinner = ({
+  width = 26,
+  height = 26,
+  color = 'text-secondary',
+  animate = 'animate-spin',
+  className = '',
+}: Spinner) => {
+  return (
+    <Loader2 className={`${animate} ${color} ${className}`} style={{ width: `${width}px`, height: `${height}px` }} />
+  );
 };
 
 export default Spinner;
