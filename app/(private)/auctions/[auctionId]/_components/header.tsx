bf34ac5801a5582724c140de628a9b3de5a0ff01
Volkov <volkov.aliksandr0203@gmail.com>
2024-11-24 03:26:52 +0200

    Created auction settings header

---
+++ app/(private)/auctions/[auctionId]/_components/header.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button';
+
+const Header = () => {
+  return (
+    <div className='flex items-center justify-between'>
+      <h5 className='text-rose-500 text-3xl font-bold'>Auction setup</h5>
+      <Button variant='outline'>Publish</Button>
+    </div>
+  );
+};
+
+export default Header;
