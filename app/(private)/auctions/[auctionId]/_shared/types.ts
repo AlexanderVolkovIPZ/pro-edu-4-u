0506365d51de891a074da22661d953d03de9aed9
Volkov <volkov.aliksandr0203@gmail.com>
2024-12-20 01:56:24 +0200

    Removed types file

--- app/(private)/auctions/[auctionId]/_shared/types.ts
+++
@@ -1,8 +0,0 @@
-import { Auction, Lot } from '@prisma/client';
-
-export type AuctionData = Pick<Auction, 'id' | 'title'> & {
-  startDate: string;
-  endDate: string;
-  description: string;
-  lot: Lot[];
-};
