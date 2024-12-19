c530bd85927b843a2980090f6f1dd32ae5d71cba
Volkov <volkov.aliksandr0203@gmail.com>
2024-12-20 01:38:42 +0200

    Updated next config

--- next.config.mjs
+++ next.config.mjs
@@ -3,13 +3,33 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
+        protocol: 'http',
         hostname: 'avatars.githubusercontent.com',
         pathname: '**',
       },
       {
         protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        pathname: '**',
+      },
+      {
+        protocol: 'http',
         hostname: 'lh3.googleusercontent.com',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        pathname: '**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'res.cloudinary.com',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
         pathname: '**',
       },
     ],
