4bc5f6386d05e5808ab07068fbfbb029c08b0189
Volkov <volkov.aliksandr0203@gmail.com>
2024-12-20 02:07:43 +0200

    Updated AuctionIdPage page

--- app/(private)/auctions/[auctionId]/page.tsx
+++ app/(private)/auctions/[auctionId]/page.tsx
@@ -1,16 +1,19 @@
 'use client';
 
+import { useAuction, useUpdateAuction } from '@/app/queries/auction';
 import Container from '@/components/container';
-import DescriptionInput from './_components/description-input';
+import InputBox from '@/components/input-box';
+import { Skeleton } from '@/components/ui/skeleton';
+import { useEffect, useState } from 'react';
+import toast from 'react-hot-toast';
+import { titleSchema } from '../_shared/schemas/title-schema';
 import EndDateInput from './_components/end-date-input';
 import Header from './_components/header';
-import StartDateInput from './_components/start-date-input';
-import TitleInput from './_components/title-input';
-import { useEffect, useState } from 'react';
-import { useAuction } from '@/app/queries/auction';
-import { Skeleton } from '@/components/ui/skeleton';
 import LotInput from './_components/lot-input';
-import { AuctionData } from './_shared/types';
+import StartDateInput from './_components/start-date-input';
+import DescriptionInput from './_shared/components/description-input';
+import { BookType } from 'lucide-react';
+import { AuctionData } from '@/app/utils/type';
 
 type AuctionIdPageParams = {
   auctionId: string;
@@ -26,6 +29,7 @@ const AuctionIdPage = ({ params }: { params: AuctionIdPageParams }) => {
     lot: [],
   });
   const { data, isFetched } = useAuction<AuctionData>(params.auctionId);
+  const { mutateAsync, isPending } = useUpdateAuction(params.auctionId);
 
   useEffect(() => {
     if (isFetched && data) {
@@ -48,8 +52,49 @@ const AuctionIdPage = ({ params }: { params: AuctionIdPageParams }) => {
           {isFetched ? (
             <>
               <div className='flex flex-col gap-y-6'>
-                <TitleInput initialTitle={auction.title} auctionId={params.auctionId} />
-                <DescriptionInput initialDescription={auction.description} auctionId={params.auctionId} />
+                <InputBox
+                  initialValue={auction.title}
+                  title='Title'
+                  fieldName='title'
+                  schema={titleSchema}
+                  isLoading={isPending}
+                  icon={BookType}
+                  registerOptions={{ required: true }}
+                  inputProps={{
+                    required: true,
+                  }}
+                  onSubmit={async (title) => {
+                    await mutateAsync({ title });
+                  }}
+                  onSuccess={() => {
+                    toast.success('The title has been successfully updated', {
+                      style: {
+                        textAlign: 'center',
+                      },
+                    });
+                  }}
+                  onError={() => {
+                    toast.error('Something went wrong');
+                  }}
+                />
+                <DescriptionInput
+                  initialDescription={auction.description}
+                  isPending={isPending}
+                  onSubmit={async (description) => {
+                    await mutateAsync({ description });
+                  }}
+                  onSuccess={() => {
+                    toast.success('The description has been successfully updated', {
+                      style: {
+                        textAlign: 'center',
+                      },
+                    });
+                  }}
+                  onError={() => {
+                    toast.error('Something went wrong');
+                  }}
+                />
+                <LotInput auctionData={auction} />
               </div>
               <div className='flex flex-col gap-y-6'>
                 <StartDateInput
@@ -62,9 +107,6 @@ const AuctionIdPage = ({ params }: { params: AuctionIdPageParams }) => {
                   initialEndDate={auction.endDate}
                   auctionId={params.auctionId}
                 />
-              </div>
-              <div className='flex flex-col gap-y-6'>
-                <LotInput auctionData={auction} />
               </div>
             </>
           ) : (
