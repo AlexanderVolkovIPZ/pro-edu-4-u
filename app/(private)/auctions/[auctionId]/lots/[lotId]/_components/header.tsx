3c63b262021b98903123ea7e1d0e6723d2929ef5
Volkov <volkov.aliksandr0203@gmail.com>
2024-12-26 18:31:53 +0200

    Updated lot page header component

--- app/(private)/auctions/[auctionId]/lots/[lotId]/_components/header.tsx
+++ app/(private)/auctions/[auctionId]/lots/[lotId]/_components/header.tsx
@@ -1,9 +1,31 @@
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from '@/components/ui/breadcrumb';
 import { Button } from '@/components/ui/button';
+import { useRouter } from 'next/navigation';
 
-const Header = () => {
+const Header = ({ auctionLink }: { auctionLink: string }) => {
+  const router = useRouter();
+
   return (
     <div className='flex items-center justify-between'>
       <h5 className='text-rose-500 text-3xl font-bold'>Lot setup</h5>
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem className='cursor-pointer'>
+            <BreadcrumbLink onClick={() => router.push(auctionLink)}>Auction</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>Lot</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
       <Button variant='outline'>Publish</Button>
     </div>
   );
