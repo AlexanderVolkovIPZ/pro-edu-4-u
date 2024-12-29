5cf27bb22d90882bb31558fab0b5a7e346d07b11
Volkov <volkov.aliksandr0203@gmail.com>
2024-12-29 18:24:19 +0200

    Removed file with AuctionWithRelationsType type

--- app/(private)/types.ts
+++
@@ -1,6 +0,0 @@
-import { Auction, Category, Lot, LotCategory, Photo, UserAuction } from '@prisma/client';
-
-export type AuctionWithRelationsType = Auction & {
-  lot: (Lot & { photo: Photo[]; lotCategory: (LotCategory & { category: Category })[] })[];
-  userAuction: UserAuction[];
-};
