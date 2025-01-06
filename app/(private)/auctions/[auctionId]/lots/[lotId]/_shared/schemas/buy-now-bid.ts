99989de9202f5c6b2db87b00a03a95809a108ae4
Volkov <volkov.aliksandr0203@gmail.com>
2025-01-07 00:17:51 +0200

    Updated BuyNowBid schema

--- app/(private)/auctions/[auctionId]/lots/[lotId]/_shared/schemas/buy-now-bid.ts
+++ app/(private)/auctions/[auctionId]/lots/[lotId]/_shared/schemas/buy-now-bid.ts
@@ -6,8 +6,6 @@ export const buyNowBidSchema = z.object({
       required_error: 'Buy now bid is required',
       invalid_type_error: 'Buy now bid must be a number',
     })
-    .nullable()
-    .refine((value) => value === null || value >= 0, {
-      message: 'Buy now bid must be greater than or equal to 0',
-    }),
+    .min(0)
+    .nullish(),
 });
