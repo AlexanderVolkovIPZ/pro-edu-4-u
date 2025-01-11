dc4fa6b7cd410aca9a7325208f30962cdec08f4c
Volkov <volkov.aliksandr0203@gmail.com>
2025-01-11 23:33:07 +0200

    Updated useLotDate hook

--- app/hooks/use-lot-date.ts
+++ app/hooks/use-lot-date.ts
@@ -25,22 +25,30 @@ export const useLotDate = ({ auctionStartDate, auctionEndDate, position, lotsCou
   const calculateTimeLeft = useCallback(() => {
     if (!auctionStartDate || !auctionEndDate || position <= 0 || lotsCount <= 0) {
       return {
-        timeLeft: { days: 0, hours: 0, minutes: 0, seconds: 0 },
+        timeStartLeft: { days: 0, hours: 0, minutes: 0, seconds: 0 },
+        timeEndLeft: { days: 0, hours: 0, minutes: 0, seconds: 0 },
         startDate: '',
         endDate: '',
       };
     }
 
     const now = +new Date();
-    const difference = +new Date(auctionStartDate) - now;
+    const auctionDateStart = +new Date(auctionStartDate) - now;
+    const lotDateStart = auctionDateStart + (position - 1) * (oneLotPeriod + ONE_MINUTE_BUFFER_TIME_BETWEEN_LOTS);
+    const lotDateEnd = lotDateStart + oneLotPeriod;
 
-    const lotDifference = difference + (position - 1) * (oneLotPeriod + ONE_MINUTE_BUFFER_TIME_BETWEEN_LOTS);
+    const timeStartLeft: TimeLeftType = {
+      days: Math.max(0, Math.floor(lotDateStart / (1000 * 60 * 60 * 24))),
+      hours: Math.max(0, Math.floor((lotDateStart / (1000 * 60 * 60)) % 24)),
+      minutes: Math.max(0, Math.floor((lotDateStart / 1000 / 60) % 60)),
+      seconds: Math.max(0, Math.floor((lotDateStart / 1000) % 60)),
+    };
 
-    const timeLeft: TimeLeftType = {
-      days: Math.max(0, Math.floor(lotDifference / (1000 * 60 * 60 * 24))),
-      hours: Math.max(0, Math.floor((lotDifference / (1000 * 60 * 60)) % 24)),
-      minutes: Math.max(0, Math.floor((lotDifference / 1000 / 60) % 60)),
-      seconds: Math.max(0, Math.floor((lotDifference / 1000) % 60)),
+    const timeEndLeft: TimeLeftType = {
+      days: Math.max(0, Math.floor(lotDateEnd / (1000 * 60 * 60 * 24))),
+      hours: Math.max(0, Math.floor((lotDateEnd / (1000 * 60 * 60)) % 24)),
+      minutes: Math.max(0, Math.floor((lotDateEnd / 1000 / 60) % 60)),
+      seconds: Math.max(0, Math.floor((lotDateEnd / 1000) % 60)),
     };
 
     const startDate = new Date(
@@ -52,7 +60,8 @@ export const useLotDate = ({ auctionStartDate, auctionEndDate, position, lotsCou
     ).toISOString();
 
     return {
-      timeLeft,
+      timeStartLeft,
+      timeEndLeft,
       startDate,
       endDate,
     };
@@ -69,7 +78,8 @@ export const useLotDate = ({ auctionStartDate, auctionEndDate, position, lotsCou
   }, [calculateTimeLeft]);
 
   return {
-    timeLeft: timeLeft.timeLeft,
+    timeStartLeft: timeLeft.timeStartLeft,
+    timeEndLeft: timeLeft.timeEndLeft,
     startDate: timeLeft.startDate,
     endDate: timeLeft.endDate,
   };
