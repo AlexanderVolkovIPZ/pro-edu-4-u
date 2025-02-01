fa35d4a245785f87885ea63939bd090de89e96aa
Volkov <volkov.aliksandr0203@gmail.com>
2025-02-01 22:37:58 +0200

    Updated 404 page font style

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,8 +6,8 @@ export default function NotFound() {
     <div className='flex items-center justify-center min-h-screen bg-white'>
       <div className='text-center'>
         <FrownIcon className='mx-auto h-16 w-16 text-rose-500 mb-4 animate-bounce' />
-        <h1 className='text-4xl font-bold text-gray-900 mb-4'>404 - Page Not Found</h1>
-        <p className='text-xl text-gray-600 mb-8'>Oops! The page you&apos;re looking for doesn&apos;t exist.</p>
+        <h1 className='text-4xl font-bold text-slate-800 mb-4'>404 - Page Not Found</h1>
+        <p className='text-xl text-slate-600 mb-8'>Oops! The page you&apos;re looking for doesn&apos;t exist.</p>
         <Link
           href='/'
           className='inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-md text-white bg-rose-500 hover:bg-rose-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-rose-500 transition-colors duration-300'
