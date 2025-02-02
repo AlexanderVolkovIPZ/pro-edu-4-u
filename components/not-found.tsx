f044ea6b62281817c4b063d7e9f7b0804813cfd1
Volkov <volkov.aliksandr0203@gmail.com>
2025-02-02 04:23:08 +0200

    Created NotFound component

---
+++ components/not-found.tsx
@@ -0,0 +1,22 @@
+import { FrownIcon } from 'lucide-react';
+import Link from 'next/link';
+
+const NotFound = () => {
+  return (
+    <div className='flex items-center justify-center bg-white mt-20'>
+      <div className='text-center'>
+        <FrownIcon className='mx-auto h-16 w-16 text-rose-500 mb-4 animate-bounce' />
+        <h1 className='text-4xl font-bold text-slate-700 mb-4'>404 - Page Not Found</h1>
+        <p className='text-xl text-slate-600 mb-8'>Oops! The page you&apos;re looking for doesn&apos;t exist.</p>
+        <Link
+          href='/'
+          className='inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-md text-white bg-rose-500 hover:bg-rose-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-rose-500 transition-colors duration-300'
+        >
+          Go Back Home
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
