0b4a6209f21e1316fab3515e2380ebdb3a29dece
Volkov <volkov.aliksandr0203@gmail.com>
2025-02-10 01:38:29 +0200

    Created LotOverviewInfo type

---
+++ app/(private)/auctions/[auctionId]/lots/[lotId]/overview/_components/lot-overview/right-column/types.ts
@@ -0,0 +1,12 @@
+import { Bid, Category, Lot, User } from '@prisma/client';
+
+export type LotOverviewInfo = {
+  categories?: {
+    categoryId: Category['id'];
+    category: Category;
+  }[];
+} & Pick<Lot, 'startBid' | 'buyNowBid' | 'minBidIncrement' | 'description' | 'id' | 'isSold'> & {
+    bids?: (Bid & {
+      user: Pick<User, 'id' | 'name'>;
+    })[];
+  };
