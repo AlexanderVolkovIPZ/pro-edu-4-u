b88d3256679ee01e2b8ba865c56632cf50b8ce1a
Volkov <volkov.aliksandr0203@gmail.com>
2025-02-10 01:43:13 +0200

    Created StatusBadge component

---
+++ app/(private)/auctions/[auctionId]/lots/[lotId]/overview/_components/lot-overview/right-column/bid-process-info/status-badge/index.tsx
@@ -0,0 +1,22 @@
+import { cn } from '@/lib/utils';
+
+type LotStatus = 'UPCOMING' | 'IN_PROGRESS' | 'COMPLETED';
+
+const STATUS_MESSAGES = {
+  UPCOMING: 'Bidding process starting soon',
+  IN_PROGRESS: 'Bidding in progress',
+  COMPLETED: 'Bid process finished',
+};
+
+export const StatusBadge = ({ status }: { status: LotStatus }) => {
+  return (
+    <div
+      className={cn(
+        'text-center font-medium p-2 rounded-md',
+        status === 'IN_PROGRESS' ? 'bg-blue-100 text-blue-800' : 'bg-red-100 text-red-800'
+      )}
+    >
+      {STATUS_MESSAGES[status]}
+    </div>
+  );
+};
