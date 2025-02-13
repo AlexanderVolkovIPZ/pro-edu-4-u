f7a4e94f10a860f5242067ddbfb2e361c8abd4b1
Volkov <volkov.aliksandr0203@gmail.com>
2025-02-14 01:57:23 +0200

    Deleted redundant files

--- app/(private)/auctions/[auctionId]/lots/[lotId]/overview/_components/header.tsx
+++
@@ -1,9 +0,0 @@
-const Header = () => {
-  return (
-    <div className='flex items-center justify-between'>
-      <h5 className='text-rose-500 text-3xl font-bold'>Auction lot</h5>
-    </div>
-  );
-};
-
-export default Header;
