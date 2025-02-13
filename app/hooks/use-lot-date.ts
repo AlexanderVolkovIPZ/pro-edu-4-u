daf7c63e2c81a51380dd9dd361e552def999f1d3
Volkov <volkov.aliksandr0203@gmail.com>
2025-02-14 01:38:21 +0200

    Created useLotDate hook

--- app/hooks/use-lot-date.ts
+++ app/hooks/use-lot-date.ts
@@ -2,10 +2,10 @@ import { Lot } from '@prisma/client';
 import { useCallback, useEffect, useState } from 'react';
 
 type UseLotDateProps = {
-  auctionStartDate: string;
-  auctionEndDate: string;
-  position: Lot['position'];
-  lotsCount: number;
+  auctionStartDate?: string;
+  auctionEndDate?: string;
+  position?: Lot['position'];
+  lotsCount?: number;
 };
 
 export type TimeLeftType = {
@@ -17,57 +17,44 @@ export type TimeLeftType = {
 
 const ONE_MINUTE_BUFFER_TIME_BETWEEN_LOTS = 60 * 1000;
 
-export const useLotDate = ({ auctionStartDate, auctionEndDate, position, lotsCount }: UseLotDateProps) => {
-  const auctionPeriodWithoutBufferTime =
-    +new Date(auctionEndDate) - +new Date(auctionStartDate) - ONE_MINUTE_BUFFER_TIME_BETWEEN_LOTS * (lotsCount - 1);
-  const oneLotPeriod = Math.floor(auctionPeriodWithoutBufferTime / lotsCount);
+const calculateTime = (timeInMs: number): TimeLeftType => ({
+  days: Math.max(0, Math.floor(timeInMs / (1000 * 60 * 60 * 24))),
+  hours: Math.max(0, Math.floor((timeInMs / (1000 * 60 * 60)) % 24)),
+  minutes: Math.max(0, Math.floor((timeInMs / 1000 / 60) % 60)),
+  seconds: Math.max(0, Math.floor((timeInMs / 1000) % 60)),
+});
+
+export const useLotDate = (props: UseLotDateProps) => {
+  const { auctionStartDate, auctionEndDate, position, lotsCount } = props;
 
   const calculateTimeLeft = useCallback(() => {
-    if (!auctionStartDate || !auctionEndDate || position <= 0 || lotsCount <= 0) {
-      return {
-        timeStartLeft: { days: 0, hours: 0, minutes: 0, seconds: 0 },
-        timeEndLeft: { days: 0, hours: 0, minutes: 0, seconds: 0 },
-        startDate: '',
-        endDate: '',
-      };
+    if (!auctionStartDate || !auctionEndDate || !position || !lotsCount) {
+      return {};
     }
 
     const now = +new Date();
-    const auctionDateStart = +new Date(auctionStartDate) - now;
-    const lotDateStart = auctionDateStart + (position - 1) * (oneLotPeriod + ONE_MINUTE_BUFFER_TIME_BETWEEN_LOTS);
+    const startTimestamp = +new Date(auctionStartDate);
+    const endTimestamp = +new Date(auctionEndDate);
+    const auctionPeriodWithoutBufferTime =
+      endTimestamp - startTimestamp - ONE_MINUTE_BUFFER_TIME_BETWEEN_LOTS * (lotsCount - 1);
+    const oneLotPeriod = Math.floor(auctionPeriodWithoutBufferTime / lotsCount);
+
+    const lotDateStart = startTimestamp - now + (position - 1) * (oneLotPeriod + ONE_MINUTE_BUFFER_TIME_BETWEEN_LOTS);
     const lotDateEnd = lotDateStart + oneLotPeriod;
 
-    const timeStartLeft: TimeLeftType = {
-      days: Math.max(0, Math.floor(lotDateStart / (1000 * 60 * 60 * 24))),
-      hours: Math.max(0, Math.floor((lotDateStart / (1000 * 60 * 60)) % 24)),
-      minutes: Math.max(0, Math.floor((lotDateStart / 1000 / 60) % 60)),
-      seconds: Math.max(0, Math.floor((lotDateStart / 1000) % 60)),
-    };
-
-    const timeEndLeft: TimeLeftType = {
-      days: Math.max(0, Math.floor(lotDateEnd / (1000 * 60 * 60 * 24))),
-      hours: Math.max(0, Math.floor((lotDateEnd / (1000 * 60 * 60)) % 24)),
-      minutes: Math.max(0, Math.floor((lotDateEnd / 1000 / 60) % 60)),
-      seconds: Math.max(0, Math.floor((lotDateEnd / 1000) % 60)),
-    };
-
-    const startDate = new Date(
-      +new Date(auctionStartDate) + (ONE_MINUTE_BUFFER_TIME_BETWEEN_LOTS + oneLotPeriod) * (position - 1)
-    ).toISOString();
-
-    const endDate = new Date(
-      +new Date(auctionStartDate) + (ONE_MINUTE_BUFFER_TIME_BETWEEN_LOTS + oneLotPeriod) * (position - 1) + oneLotPeriod
-    ).toISOString();
-
     return {
-      timeStartLeft,
-      timeEndLeft,
-      startDate,
-      endDate,
+      timeStartLeft: calculateTime(lotDateStart),
+      timeEndLeft: calculateTime(lotDateEnd),
+      startDate: new Date(
+        startTimestamp + (ONE_MINUTE_BUFFER_TIME_BETWEEN_LOTS + oneLotPeriod) * (position - 1)
+      ).toISOString(),
+      endDate: new Date(
+        startTimestamp + (ONE_MINUTE_BUFFER_TIME_BETWEEN_LOTS + oneLotPeriod) * (position - 1) + oneLotPeriod
+      ).toISOString(),
     };
-  }, [auctionEndDate, auctionStartDate, lotsCount, oneLotPeriod, position]);
+  }, [auctionEndDate, auctionStartDate, lotsCount, position]);
 
-  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft);
 
   useEffect(() => {
     const interval = setInterval(() => {
