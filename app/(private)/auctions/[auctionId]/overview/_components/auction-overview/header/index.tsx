d13a336cc7fe82eeebee87d359c9f55c8615a788
Volkov <volkov.aliksandr0203@gmail.com>
2025-02-14 02:25:38 +0200

    Updated header

--- app/(private)/auctions/[auctionId]/overview/_components/auction-overview/header/index.tsx
+++ app/(private)/auctions/[auctionId]/overview/_components/auction-overview/header/index.tsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <div className='flex items-center justify-between'>
-      <h5 className='text-rose-500 text-3xl font-bold'>Auction lots</h5>
+      <h5 className='text-rose-500 text-3xl font-bold'>Auction Lots</h5>
     </div>
   );
 };
