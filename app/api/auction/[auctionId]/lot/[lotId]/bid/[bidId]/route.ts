7aabba959ef222a00a32b3c3c8f06e4e4df3b002
Volkov <volkov.aliksandr0203@gmail.com>
2025-02-14 21:23:54 +0200

    Created BidId route

---
+++ app/api/auction/[auctionId]/lot/[lotId]/bid/[bidId]/route.ts
@@ -0,0 +1,36 @@
+import getAuthUser from '@/app/actions/get-auth-user';
+import { Bid } from '@prisma/client';
+import { NextResponse } from 'next/server';
+
+export async function PATCH(
+  request: Request,
+  { params }: { params: { auctionId: string; lotId: string; bidId: string } }
+) {
+  const authUser = await getAuthUser();
+  if (!authUser) {
+    return new NextResponse('Unauthorized', { status: 401 });
+  }
+
+  const body: Omit<Bid, 'id' | 'createdAt'> = await request.json();
+  const { amount, bidderId, isWinner, lotId } = body;
+
+  try {
+    const bid = await prismaDb?.bid.update({
+      data: {
+        amount,
+        bidderId,
+        isWinner,
+        lotId,
+      },
+      where: {
+        id: params.bidId,
+        lotId: params.lotId,
+      },
+    });
+
+    return NextResponse.json(bid);
+  } catch (error) {
+    console.error('UPDATE_LOT_ERROR -> ', error);
+    return new NextResponse('Internal server error', { status: 500 });
+  }
+}
