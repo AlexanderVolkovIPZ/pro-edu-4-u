52ff74ebb54f8f421387f3c14d7f8c781c07cac3
Volkov <volkov.aliksandr0203@gmail.com>
2025-02-25 01:34:30 +0200

    Created Payment component

---
+++ app/(private)/won-lots/payment/page.tsx
@@ -0,0 +1,14 @@
+import Container from '@/components/container';
+import CheckoutPage from './_components/checkout';
+import Header from './_components/header';
+
+const Payment = () => {
+  return (
+    <Container>
+      <Header />
+      <CheckoutPage />
+    </Container>
+  );
+};
+
+export default Payment;
