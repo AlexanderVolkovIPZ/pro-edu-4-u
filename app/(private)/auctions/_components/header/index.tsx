f392b38137507d5ff089423bf95affef39366b00
Volkov <volkov.aliksandr0203@gmail.com>
2025-03-18 02:59:45 +0200

    Updated Header

--- app/(private)/auctions/_components/header/index.tsx
+++ app/(private)/auctions/_components/header/index.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { Button } from '@/components/ui/button';
-import { CardHeader, CardTitle } from '@/components/ui/card';
 import { PlusCircle } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 
@@ -9,14 +8,12 @@ const Header = () => {
   const router = useRouter();
 
   return (
-    <CardHeader className='p-0'>
-      <div className='flex justify-between items-center gap-x-2'>
-        <CardTitle>Auction Management</CardTitle>
-        <Button onClick={() => router.push('/auctions/create')}>
-          <PlusCircle className='mr-2 h-4 w-4' /> New Auction
-        </Button>
-      </div>
-    </CardHeader>
+    <div className='flex justify-between items-center gap-x-2'>
+      <h5 className='text-rose-500 text-3xl font-bold'>Auction Management</h5>
+      <Button onClick={() => router.push('/auctions/create')}>
+        <PlusCircle className='mr-2 h-4 w-4' /> New Auction
+      </Button>
+    </div>
   );
 };
 
