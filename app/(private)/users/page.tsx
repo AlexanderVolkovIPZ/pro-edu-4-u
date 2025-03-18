5d3e9c9c143e625bcf4479bfa0a94f5beef13bc3
Volkov <volkov.aliksandr0203@gmail.com>
2025-03-18 02:54:05 +0200

    Created Table component

---
+++ app/(private)/users/page.tsx
@@ -0,0 +1,14 @@
+import Container from '@/components/container';
+import Header from './_components/header';
+import Table from './_components/table';
+
+const UsersPage = () => {
+  return (
+    <Container>
+      <Header />
+      <Table />
+    </Container>
+  );
+};
+
+export default UsersPage;
