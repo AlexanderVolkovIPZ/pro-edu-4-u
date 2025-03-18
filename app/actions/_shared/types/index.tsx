18809c486fbdb14877279be2595087bb569ef535
Volkov <volkov.aliksandr0203@gmail.com>
2025-03-18 02:26:38 +0200

    Updated GetServerSessionParams location

--- app/actions/_shared/types/index.tsx
+++
@@ -1,6 +0,0 @@
-import { GetServerSidePropsContext, NextApiRequest, NextApiResponse } from 'next/types';
-
-export type GetServerSessionParams =
-  | [GetServerSidePropsContext['req'], GetServerSidePropsContext['res']]
-  | [NextApiRequest, NextApiResponse]
-  | [];
