f2b6485a6a047d70928d1762bb9010eeb52a8df4
Volkov <volkov.aliksandr0203@gmail.com>
2025-04-01 02:18:14 +0300

    Updated AvatarProps type

--- components/avatar.tsx
+++ components/avatar.tsx
@@ -1,7 +1,7 @@
 import { Avatar as UserAvatar, AvatarImage } from '@/components/ui/avatar';
 
 type AvatarProps = {
-  url: string;
+  url?: string | null;
   width?: number;
   height?: number;
 };
@@ -12,7 +12,7 @@ const Avatar = ({ url, width = 28, height = 28 }: AvatarProps) => {
       style={{ width: `${width}px`, height: `${height}px` }}
       className={` border border-slate-300 cursor-pointer rounded-full`}
     >
-      <AvatarImage src={url} />
+      <AvatarImage src={url ?? undefined} />
     </UserAvatar>
   );
 };
