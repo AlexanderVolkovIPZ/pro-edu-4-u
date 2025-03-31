5d23a08946cef5180c264bbb67896d9089981d82
Volkov <volkov.aliksandr0203@gmail.com>
2025-04-01 02:31:09 +0300

    Created shippingId route

---
+++ app/api/shipping/[shippingId]/route.ts
@@ -0,0 +1,34 @@
+import getAuthUser from '@/app/actions/get-auth-user';
+import { Shipping, UserRole } from '@prisma/client';
+import { NextResponse } from 'next/server';
+
+export async function PATCH(request: Request, { params }: { params: { shippingId: string } }) {
+  try {
+    const authUser = await getAuthUser();
+    if (!authUser) {
+      return new NextResponse('Unauthorized', { status: 401 });
+    }
+
+    const isAuthUserAdmin = authUser.role === UserRole.ADMIN;
+    if (!isAuthUserAdmin) {
+      return new NextResponse('Auction not found', { status: 404 });
+    }
+
+    const body = await request.json();
+
+    delete body.id;
+    const data: Partial<Omit<Shipping, 'id'>> = body;
+
+    const shipping = await prismaDb?.shipping.update({
+      data,
+      where: {
+        id: params.shippingId,
+      },
+    });
+
+    return NextResponse.json(shipping);
+  } catch (error) {
+    console.error('UPDATE_SHIPPING_ERROR -> ', error);
+    return new NextResponse('Internal server error', { status: 500 });
+  }
+}
