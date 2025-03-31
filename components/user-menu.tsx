63266a028ddb86500ad54636f1e61f07048d4351
Volkov <volkov.aliksandr0203@gmail.com>
2025-04-01 02:19:46 +0300

    Updated value

--- components/user-menu.tsx
+++ components/user-menu.tsx
@@ -15,7 +15,7 @@ const UserMenu = ({ url, email }: UserMenuProps) => {
     <Popover>
       <PopoverTrigger className='flex items-center gap-x-2 px-2 py-1 border-[1px] border-slate-300  rounded-full transition hover:shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]'>
         <Menu width={20} height={20} />
-        <Avatar url={url ?? 'https://avatar.iran.liara.run/public'} width={26} height={26} />
+        <Avatar url={url} width={26} height={26} />
       </PopoverTrigger>
       <PopoverContent align='end' className='min-w-32 max-w-min p-1'>
         <div className='py-2 px-2'>
