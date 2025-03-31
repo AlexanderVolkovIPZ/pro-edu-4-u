ad49399fe3e8548ef0d2991704c0fefd3b6780c4
Volkov <volkov.aliksandr0203@gmail.com>
2025-04-01 02:27:15 +0300

    Created orderTabsList list

---
+++ app/(private)/orders/_shared/lists/order-tab-list.ts
@@ -0,0 +1,17 @@
+export const orderTabsList: {
+  name: string;
+  route: string;
+}[] = [
+  {
+    name: 'All Orders',
+    route: 'all-orders',
+  },
+  {
+    name: 'My Orders',
+    route: 'my-orders',
+  },
+  {
+    name: 'Sent Orders',
+    route: 'sent-orders',
+  },
+];
