2f527c593d8f84742c0fe722c18514809ce068cb
Volkov <volkov.aliksandr0203@gmail.com>
2025-04-01 02:27:37 +0300

    Created Header component

---
+++ app/(private)/orders/_components/header/index.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className='flex justify-between items-center gap-x-2'>
+      <h5 className='text-rose-500 text-3xl font-bold'>Orders</h5>
+    </div>
+  );
+};
+
+export default Header;
