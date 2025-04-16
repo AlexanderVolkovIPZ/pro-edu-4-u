137a59609ebfffbff0fa70b3ac7b9b73aeae798e
Volkov <volkov.aliksandr0203@gmail.com>
2025-04-17 02:14:19 +0300

    Created Header component

--- app/(private)/_components/main/header/index.tsx
+++ app/(private)/_components/main/header/index.tsx
@@ -1,7 +1,11 @@
-const Header = () => {
+import { AuctionFiltering } from '../_shared/types';
+import FilterMenu from './filter-menu';
+
+const Header = ({ data, isFetched, onChangeFilters }: AuctionFiltering) => {
   return (
     <div className='flex items-center justify-between'>
       <h5 className='text-rose-500 text-3xl font-bold'>Auctions</h5>
+      <FilterMenu data={data} isFetched={isFetched} onChangeFilters={onChangeFilters} />
     </div>
   );
 };
