5f0519c67340333bc9dfe9164874f8752feae7eb
Volkov <volkov.aliksandr0203@gmail.com>
2025-04-17 02:16:40 +0300

    Updated Main component

--- app/(private)/_components/main/index.tsx
+++ app/(private)/_components/main/index.tsx
@@ -20,19 +20,44 @@ const Main = () => {
     isPublished: true,
     page: 1,
     limit: PAGE_ITEMS_LIMIT,
+    minLotPrice: undefined,
+    maxLotPrice: undefined,
+    categories: undefined,
   });
-  const { data: { auctions = [], total, totalPages } = {}, isFetched } = useAuctionsByFilter<AuctionWithRelationsType>({
+  const {
+    data: {
+      auctions = [],
+      total,
+      totalPages,
+      maxLotPriceExisted = 0,
+      minLotPriceExisted = 0,
+      lotCategoriesExistedNames = [],
+    } = {},
+    isFetched,
+  } = useAuctionsByFilter<AuctionWithRelationsType>({
     filters: {
       isPublished: params.isPublished,
       page: params.page,
       limit: params.limit,
+      minLotPrice: params.minLotPrice,
+      maxLotPrice: params.maxLotPrice,
+      categories: params.categories,
     },
     options: {
       staleTime: 1000 * 60 * 3,
     },
   });
 
-  const handlePageChange = (newPage: number) => {
+  const maxLotPriceSelected = params.maxLotPrice
+    ? params.maxLotPrice
+    : Math.max(...auctions.map((auction) => auction.lot.reduce((max, lot) => Math.max(max, lot.startBid ?? 0), 0)));
+  const minLotPriceSelected = params.minLotPrice
+    ? params.minLotPrice
+    : Math.min(...auctions.map((auction) => auction.lot.reduce((min, lot) => Math.min(min, lot.startBid ?? 0), 0)));
+
+  const lotCategoriesSelectedNames = params.categories ? JSON.parse(params.categories) : [];
+
+  const onPageChange = (newPage: number) => {
     setParams({ ...params, page: newPage });
     window.scrollTo({ top: 0, behavior: 'smooth' });
   };
@@ -53,7 +78,22 @@ const Main = () => {
 
   return (
     <Container>
-      <Header />
+      <Header
+        onChangeFilters={({ priceRange: { minPrice, maxPrice }, categories }) =>
+          setParams({ ...params, minLotPrice: minPrice, maxLotPrice: maxPrice, categories })
+        }
+        data={{
+          maxLotPriceExisted,
+          minLotPriceExisted,
+          maxLotPriceSelected,
+          minLotPriceSelected,
+          lotCategoriesExistedNames,
+          lotCategoriesSelectedNames,
+        }}
+        isFetched={isFetched}
+        key={isFetched.toString()} //To force re-render
+      />
+
       <main className='mt-2 flex flex-col'>
         <div className='flex flex-wrap gap-3 lg:gap-4 flex-grow'>
           {isFetched ? (
@@ -72,7 +112,7 @@ const Main = () => {
             className='mt-4'
             currentPage={params.page}
             totalPages={totalPages || 1}
-            onPageChange={handlePageChange}
+            onPageChange={onPageChange}
             showSummary={false}
             itemsPerPage={PAGE_ITEMS_LIMIT}
             totalItems={total || 0}
