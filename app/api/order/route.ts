3853b085dac88179fc35bf6f40776d7376e89d13
Volkov <volkov.aliksandr0203@gmail.com>
2025-04-25 00:50:51 +0300

    Updated GET /order route

--- app/api/order/route.ts
+++ app/api/order/route.ts
@@ -1,5 +1,5 @@
 import getAuthUser from '@/app/actions/get-auth-user';
-import { AuctionRole, UserRole } from '@prisma/client';
+import { AuctionRole, ShippingStatus, UserRole } from '@prisma/client';
 import { NextResponse } from 'next/server';
 
 export async function GET(request: Request) {
@@ -32,6 +32,9 @@ export async function GET(request: Request) {
               },
             },
           },
+        },
+        status: {
+          not: ShippingStatus.DELIVERED,
         },
       };
       totalCount = await prismaDb?.shipping.count({
