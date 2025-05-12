11422e2c67a8e4751d78cdef3e430fdb1c9ea98e
Volkov <volkov.aliksandr0203@gmail.com>
2025-05-13 01:23:52 +0300

    Updated statuses

--- app/utils/get-lot-status.ts
+++ app/utils/get-lot-status.ts
@@ -1,15 +1,15 @@
-import { LotStatus } from '../constants';
+import { LOT_STATUSES } from '../constants';
 
 const getLotStatus = (startDate?: string, endDate?: string, isLotSold?: boolean) => {
-  if (!startDate || !endDate) return LotStatus.UPCOMING;
+  if (!startDate || !endDate) return LOT_STATUSES.UPCOMING;
 
   const now = +new Date();
   const start = +new Date(startDate);
   const end = +new Date(endDate);
 
-  if (isLotSold || now > end) return LotStatus.COMPLETED;
-  if (!isLotSold && now < end && now > start) return LotStatus.IN_PROGRESS;
-  return LotStatus.UPCOMING;
+  if (isLotSold || now > end) return LOT_STATUSES.COMPLETED;
+  if (!isLotSold && now < end && now > start) return LOT_STATUSES.IN_PROGRESS;
+  return LOT_STATUSES.UPCOMING;
 };
 
 export default getLotStatus;
