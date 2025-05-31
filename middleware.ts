815596b0e6fc1854792cedd2baedf9eaf9675f30
Volkov <volkov.aliksandr0203@gmail.com>
2025-05-31 03:59:40 +0300

    Added request.url

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,18 @@
+import { NextRequest } from 'next/server';
 import getAuthUser from './app/actions/get-auth-user';
 import { NextResponse } from 'next/server';
 
 export const config = { matcher: ['/((?!sign-in|sign-up|(?!ws$)).*)'] };
 
-export async function middleware() {
+export async function middleware(request: NextRequest) {
   try {
     const authUser = await getAuthUser();
 
     if (!authUser || !authUser.isActive) {
-      return NextResponse.redirect(new URL('/sign-in'));
+      return NextResponse.redirect(new URL('/sign-in', request.url));
     }
     return NextResponse.next();
   } catch {
-    return NextResponse.redirect(new URL('/sign-in'));
+    return NextResponse.redirect(new URL('/sign-in', request.url));
   }
 }
