2d0d2f617c5664e2fd65ebc24be212158f95f472
Volkov <volkov.aliksandr0203@gmail.com>
2025-06-07 19:50:29 +0300

    Hide unused pages

--- app/(private)/_shared/lists/sidebar-list.ts
+++ app/(private)/_shared/lists/sidebar-list.ts
@@ -17,7 +17,7 @@ export const userSidebarList = [
     icon: Truck,
   },
   {
-    link: '/settings',
+    link: '/settings/account',
     name: 'main.settings',
     icon: Settings,
   },
@@ -50,7 +50,7 @@ export const adminSidebarList = [
     icon: Truck,
   },
   {
-    link: '/settings',
+    link: '/settings/account',
     name: 'main.settings',
     icon: Settings,
   },
