79e71b629d5caad893163dc2ca5600bc48bba5f5
Volkov <volkov.aliksandr0203@gmail.com>
2025-06-07 19:53:35 +0300

    Updated item navigation

--- components/user-menu.tsx
+++ components/user-menu.tsx
@@ -7,6 +7,7 @@ import { useTranslation } from 'react-i18next';
 import Avatar from './avatar';
 import { Popover, PopoverContent, PopoverTrigger } from './ui/popover';
 import UserItemMenu from './user-item-menu';
+import { useRouter } from 'next/navigation';
 
 type UserMenuProps = {
   url?: string | null;
@@ -14,6 +15,7 @@ type UserMenuProps = {
 };
 
 const UserMenu = ({ url, email }: UserMenuProps) => {
+  const router = useRouter();
   const { t } = useTranslation();
 
   return (
@@ -28,7 +30,11 @@ const UserMenu = ({ url, email }: UserMenuProps) => {
           {email && <div className='text-[10px] text-slate-700 pt-1'>{email}</div>}
         </div>
         <Separator />
-        <UserItemMenu label={t('avatar_menu.settings')} icon={Settings} onClick={() => {}} />
+        <UserItemMenu
+          label={t('avatar_menu.settings')}
+          icon={Settings}
+          onClick={() => router.push('/settings/account')}
+        />
         <Separator />
         <UserItemMenu label={t('avatar_menu.log_out')} icon={LogOut} onClick={() => signOut({})} />
       </PopoverContent>
