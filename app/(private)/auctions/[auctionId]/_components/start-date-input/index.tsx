76a95e5f0af08bffc5743195cb85c7dcfbd216ff
Volkov <volkov.aliksandr0203@gmail.com>
2025-06-17 01:04:02 +0300

    Updated localisation & timeIntervals

--- app/(private)/auctions/[auctionId]/_components/start-date-input/index.tsx
+++ app/(private)/auctions/[auctionId]/_components/start-date-input/index.tsx
@@ -104,7 +104,7 @@ const StartDateInput = ({ initialStartDate, auctionId, showRequiredFieldIcon = f
               onChange={(date) => setValue('startDate', date as Date)}
               showTimeSelect
               timeFormat={timeFormat}
-              timeIntervals={5}
+              timeIntervals={1}
               dateFormat={`${dateFormat}, ${timeFormat}`}
               minDate={new Date()}
               maxDate={dayjs(new Date()).add(1, 'year').toDate()}
